d702bde757634f3be3a91696ed03a085613b350e
bitnik93 <Nick93sysoev@yandex.ru>
2022-03-11 15:50:03 +0300

    Исправления еще одни исправления домашки 4

--- js/main.js
+++ js/main.js
@@ -12,42 +12,9 @@ const isValidLength = (userComment, maxLength) => userComment.length <= maxLengt
 isValidLength('hello', 140);
 
 
-const url = [
-  'photos/1.jpg',
-  'photos/2.jpg',
-  'photos/3.jpg',
-  'photos/4.jpg',
-  'photos/5.jpg',
-  'photos/6.jpg',
-  'photos/7.jpg',
-  'photos/8.jpg',
-  'photos/9.jpg',
-  'photos/10.jpg',
-  'photos/11.jpg',
-  'photos/12.jpg',
-  'photos/13.jpg',
-  'photos/14.jpg',
-  'photos/15.jpg',
-  'photos/16.jpg',
-  'photos/17.jpg',
-  'photos/18.jpg',
-  'photos/19.jpg',
-  'photos/20.jpg',
-  'photos/21.jpg',
-  'photos/22.jpg',
-  'photos/23.jpg',
-  'photos/24.jpg',
-  'photos/25.jpg'
-];
+const urlList = [];
 
-const avatars = [
-  'img/avatar-1.svg',
-  'img/avatar-2.svg',
-  'img/avatar-3.svg',
-  'img/avatar-4.svg',
-  'img/avatar-5.svg',
-  'img/avatar-6.svg'
-];
+const avatarsList = [];
 
 const messageList = [
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
@@ -68,19 +35,42 @@ const namesList =  [
   'Максим'
 ];
 
+const getUrl = function(urlIndex) {
+  for(let i = 0; i <urlIndex.length; i++) {
+    return Array.concat(urlIndex[i]);
+  }
+};
+
+const getAvatars = function(avatarsIndex) {
+  for(let i = 0; i <avatarsIndex.length; i++) {
+    return Array.concat(avatarsIndex[i]);
+  }
+};
+
+const getNonRepeatNumber = function(min, max) {
+  let i;
+  if (min < 0 && min >= max && (i !== i)) {
+    throw new Error('значение невалидно');
+  }
+  return Math.floor(Math.random()*(max-min) + min);
+};
+const getNonRepeatArrayNumber = (elements) => elements[getNonRepeatNumber(0, elements.length - 1)];
 const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
 const descriptionPhoto = () => ({
-  getUrl:  getRandomArrayElement(url),
+  url: getNonRepeatArrayNumber(getUrl(urlList)),
   getDesctription:  getRandomArrayElement(description),
-  getLikes: getRandomNumber(1, 100),
-  getComments: [ {
-    id: getRandomNumber(1, 25),
-    avatar:  getRandomArrayElement(avatars),
+  getLikes: getRandomNumber(15, 200),
+  comments: [ {
+    id: getNonRepeatNumber(1, 25),
+    avatar:  getRandomArrayElement(getAvatars(avatarsList)),
     message:  getRandomArrayElement(messageList),
     name:  getRandomArrayElement(namesList)
   }
   ],
 });
 
+// eslint-disable-next-line
 const similarPhotoComment = Array.from({length: 25}, descriptionPhoto);
+
+
