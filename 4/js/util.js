ec7f7690e7699bfa05f22e6536c4ae45a2e1c4f0
Keks <keks@htmlacademy.ru>
2022-03-14 20:21:20 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/util.js
@@ -0,0 +1,13 @@
+
+function getRandomNumber(min, max){
+  if (min < 0 && min >= max) {
+    throw new Error('значение невалидно');
+  }
+  return Math.floor(Math.random()*(max-min) + min);
+}
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+const isValidLength = (userComment, maxLength) => userComment.length <= maxLength;
+
+export {getRandomNumber, getRandomArrayElement, isValidLength};
