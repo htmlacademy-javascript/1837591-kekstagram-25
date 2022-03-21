be33fe7d587027ba08aab5726e1d431f33d80ea3
Keks <keks@htmlacademy.ru>
2022-03-21 16:05:07 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/thumbnails.js
+++ 5/js/thumbnails.js
@@ -3,18 +3,14 @@ import {photoContent} from './decription-photo.js';
 const picturesContainer = document.querySelector('.pictures');
 const similarPictureTemplate  = document.querySelector('#picture').content.querySelector('.picture');
 
-const pictureElement = similarPictureTemplate.cloneNode(true);
-picturesContainer.appendChild(pictureElement);
 
-const similarPhoto = photoContent();
 const similarListFragment = document.createDocumentFragment();
 
-similarPhoto.foreach((url, likes, comments) => {
+photoContent.foreach((photo) => {
   const photoElement = similarPictureTemplate.cloneNode(true);
-  photoElement.querySelector('.picture__img').src = url;
-  photoElement.querySelector('.picture__likes').textContent = likes;
-  photoElement.querySelector('.picture__comments').textContent = comments;
-  picturesContainer.appendChild(photoElement);
+  photoElement.querySelector('.picture__img').src = photo.url;
+  photoElement.querySelector('.picture__likes').textContent = photo.likes;
+  photoElement.querySelector('.picture__comments').textContent = photo.comments;
   similarListFragment.appendChild(photoElement);
 }
 );
