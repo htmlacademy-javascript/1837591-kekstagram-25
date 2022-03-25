16c58afc15341c6efa3bedfc8ef60b472c6f078a
Keks <keks@htmlacademy.ru>
2022-03-25 15:21:14 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,6 @@
+import './util.js';
+import './random-comment.js';
+import './decription-photo.js';
+import './data.js';
+import './thumbnails.js';
+import './photo-comments.js';
---
+++ 6/js/thumbnails.js
@@ -0,0 +1,21 @@
+import {photoContent} from './decription-photo.js';
+import {openPopupAndFillContent} from './photo-comments.js';
+
+const picturesContainer = document.querySelector('.pictures');
+const similarPictureTemplate  = document.querySelector('#picture').content.querySelector('.picture');
+
+
+const fragment = document.createDocumentFragment();
+
+photoContent.forEach((photo) => {
+  const photoElement = similarPictureTemplate.cloneNode(true);
+  photoElement.querySelector('.picture__img').src = photo.url;
+  photoElement.querySelector('.picture__likes').textContent = photo.likes.toString();
+  photoElement.querySelector('.picture__comments').textContent = photo.comments.length.toString();
+  photoElement.addEventListener('click', () => {
+    openPopupAndFillContent(photo);
+  });
+  fragment.appendChild(photoElement);
+});
+
+picturesContainer.appendChild(fragment);
