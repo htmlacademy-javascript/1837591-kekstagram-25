bc396f54b6fff5462d772f9e301c462babc3309e
bitnik93 <Nick93sysoev@yandex.ru>
2022-04-13 14:50:16 +0300

    правки по проекту 2

--- js/photo-comments.js
+++ js/photo-comments.js
@@ -11,7 +11,9 @@ const loadMoreButton = bigPicture.querySelector('.social__comments-loader');
 const socialCount = bigPicture.querySelector('.social__count');
 const STEP = 5;
 const MIN_COMMENTS = 5;
+let maxVisibleComments = MIN_COMMENTS;
 
+let onLoadMoreButtonClick = null;
 
 const createCommentElement = (comment) => {
   const commentElement = document.createElement('li');
@@ -32,20 +34,21 @@ const createCommentElement = (comment) => {
 
 
 const closePopup = () => {
+  maxVisibleComments = MIN_COMMENTS;
   bigPicture.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
   loadMoreButton.classList.remove('hidden');
   cancelButtonElement.removeEventListener('click', closePopup);
+  loadMoreButton.removeEventListener('click', onLoadMoreButtonClick);
 };
 
 document.addEventListener('keydown', (evt) => {
   if (evt.key === 'Escape') {
-    bigPicture.classList.add('hidden');
-    loadMoreButton.classList.remove('hidden');
+    closePopup();
   }
 });
 
-const renderComments = (comments, maxVisibleComments) => {
+const renderComments = (comments) => {
   for (let i = maxVisibleComments - STEP; i < maxVisibleComments; i++) {
     if (!comments[i]) {
       break;
@@ -54,11 +57,19 @@ const renderComments = (comments, maxVisibleComments) => {
   }
 };
 
+const loadComments = (photo) => {
+  maxVisibleComments += STEP;
+  renderComments(photo.comments);
+  socialCount.textContent = socialCommentsElements.children.length;
+  if (photo.comments.length === socialCommentsElements.children.length) {
+    loadMoreButton.classList.add('hidden');
+  }
+};
+
 const openPopupAndFillContent = (photo) => {
   if (photo.comments.length <= MIN_COMMENTS) {
     loadMoreButton.classList.add('hidden');
   }
-  let maxVisibleComments = 5;
   bodyElement.classList.add('modal-open');
   bigPicture.classList.remove('hidden');
   socialCommentsElements.innerHTML = '';
@@ -67,17 +78,10 @@ const openPopupAndFillContent = (photo) => {
   commentsCount.textContent = photo.comments.length.toString();
   descriptionElement.textContent = photo.description;
   cancelButtonElement.addEventListener('click', closePopup);
-  renderComments(photo.comments, maxVisibleComments);
+  renderComments(photo.comments);
   socialCount.textContent =  socialCommentsElements.children.length;
-  loadMoreButton.addEventListener('click', () => {
-    maxVisibleComments += 5;
-    renderComments(photo.comments, maxVisibleComments);
-    console.log(maxVisibleComments);
-    socialCount.textContent = socialCommentsElements.children.length;
-    if (photo.comments.length === socialCommentsElements.children.length) {
-      loadMoreButton.classList.add('hidden');
-    }
-  });
+  onLoadMoreButtonClick = () => loadComments(photo);
+  loadMoreButton.addEventListener('click', onLoadMoreButtonClick);
 };
 
 export {openPopupAndFillContent};
